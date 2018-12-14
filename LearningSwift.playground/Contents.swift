4e725c0d9f0d5fe1189d749528cfdb42aa5c66f9
Kavya Hanisha <gajula-uma-venkata.k@husky.neu.edu>
2018-12-13 21:08:04 -0500

    init

---
+++ LearningSwift.playground/Contents.swift
@@ -0,0 +1,37 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+print("Hello World")
+
+var month = 12
+month
+
+
+let pi = 3.14
+
+
+var sent = "My name is \"Kavya\"."
+
+
+var age = 20
+
+if (age>=5){
+    print("older")
+}
+
+
+var newArray = [Int]()
+newArray.append(1)
+newArray.append(20)
+newArray.append(100)
+
+print(newArray.count)
+
+var courses = ["programming design","algorithms","machine learning", "artificial intelligence"]
+
+print(courses.sorted())
+
+
